c94d9dccf22e1c4981802512ef2ac3047211da73
qvintius <kvint1985@mail.ru>
2023-09-30 20:37:05 +0300

    init lab3

--- src/main/java/mainpackage/lab3/exampleEvents/LogEventListener.java
+++
@@ -1,9 +0,0 @@
-package mainpackage.lab3.exampleEvents;
-
-import jakarta.enterprise.event.Observes;
-/*
-public class LogEventListener {//слушатель события
-    public void logEvent(@Observes DateSource event){
-        System.out.println("Message Date: " + event.getDate());
-    }
-}*/
--- src/main/java/mainpackage/lab3/exampleEvents/Logger.java
+++
@@ -1,5 +0,0 @@
-package mainpackage.lab3.exampleEvents;
-
-public interface Logger {
-    public void print(String message);
-}
