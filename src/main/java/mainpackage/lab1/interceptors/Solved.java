6fb581a998157fada2cc18ed018de218dcb4e464
qvintius <102350882+qvintius@users.noreply.github.com>
2023-09-30 20:04:34 +0300

    Update Solved.java

--- src/main/java/mainpackage/lab1/interceptors/Solved.java
+++ src/main/java/mainpackage/lab1/interceptors/Solved.java
@@ -1,17 +1,1 @@
-package mainpackage.lab1.interceptors;
 
-import jakarta.interceptor.InterceptorBinding;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-//@Inherited
-@InterceptorBinding
-@Target({TYPE, METHOD})
-@Retention(RUNTIME)
-public @interface Solved {//привязка перехватчика
-}
